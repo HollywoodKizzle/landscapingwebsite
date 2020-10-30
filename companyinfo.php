8a8cb238c7bab3f3482bb8adabe51c0b96d1a873
Johnny Platinum <johnnyplatinum178@gmail.com>
2020-10-30 16:42:45 -0400

    updates

--- companyinfo.php
+++ companyinfo.php
@@ -8,4 +8,4 @@ $workinghours="Mon-Sat 9am-5pm";
 
 <a href="tel:+1<?php echo $phoneinfo;?>">
 
-updatemaster git add . &&git commit -m 'updates' 
+updatemaster "!git add . &&git commit -m 'updates' &&git push AlemaSite master" 
