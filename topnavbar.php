972942f0729069f447d67182bf38a6cbb036c13f
Johnny Platinum <johnnyplatinum178@gmail.com>
2020-11-04 13:54:18 -0500

    horizontal navbar

--- topnavbar.php
+++ topnavbar.php
@@ -5,7 +5,7 @@
   <a href="/Template/projects-gallery.php">Our Work</a>
   <a href="/Template/contact.php">Contact Us</a>
   <a href="/Template/services.php" class="active">Services</a>
-  <a href="/Template/pricing.php" class="active">Pricing</a>
+  <a href="/Template/pricing.php" id="pricing-link" class="active">Pricing</a>
   <a href="/Template/request-quote.php" id="free-quotek" class="active">Free Quote</a>
 
   
