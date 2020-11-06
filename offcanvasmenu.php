35426a1b4e964cba8af37139ce19dd9a5938482f
Johnny Platinum <johnnyplatinum178@gmail.com>
2020-11-06 08:33:19 -0500

    converted links of offcanvasmenu.php into relative urls

--- offcanvasmenu.php
+++ offcanvasmenu.php
@@ -13,11 +13,11 @@
                 <span></span>
              <!--This is the off-canvas menu that appears when the Hamburger Icon is clicked-->    
             <ul id="menu" class="">
-              <li><a href="/Template/index.php">Home</a></li>
-              <li><a href="/Template/about.php">About Us</a></li>
-              <li class="debug"><a class="debug" href="/Template/projects-gallery.php">See Our Work</a></li>
-              <li><a href="/Template/contact.php">Contact</a></li>
-			  <li><a href="/Template/pricing.php">Pricing</a></li>
-			 <!-- <li><a href="/Template/request-quote.php">Free Quote</a></li>-->
+              <li><a href="index.php">Home</a></li>
+              <li><a href="about.php">About Us</a></li>
+              <li class="debug"><a class="debug" href="projects-gallery.php">See Our Work</a></li>
+              <li><a href="contact.php">Contact</a></li>
+			  <li><a href="pricing.php">Pricing</a></li>
+			 <!-- <li><a href="request-quote.php">Free Quote</a></li>-->
             </ul>
 </div>
