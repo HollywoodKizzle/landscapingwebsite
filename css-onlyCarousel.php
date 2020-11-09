0946fe39581e0fd635a29c4cc9b685149aaa2dc0
Johnny Platinum <johnnyplatinum178@gmail.com>
2020-11-09 09:05:37 -0500

    centered carousel text

--- css-onlyCarousel.php
+++ css-onlyCarousel.php
@@ -1,13 +1,13 @@
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <p class="carousel-text d-block">First Image</p>
+      <p class="carousel-text">First Image</p>
     </div>
     <div class="carousel-item">
-      <p class="carousel-text d-block">Second Image</p>
+      <p class="carousel-text">Second Image</p>
     </div>
     <div class="carousel-item">
-      <p class="carousel-text d-block">Third Image</p>
+      <p class="carousel-text">Third Image</p>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
