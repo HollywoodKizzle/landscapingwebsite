396678ab71beec4010dc1afecb6d75fab89f6118
Johnny Platinum <johnnyplatinum178@gmail.com>
2020-11-11 09:25:31 -0500

    more content revisions of index.php

--- index.php
+++ index.php
@@ -31,13 +31,12 @@ $workinghours="Mon-Sat 9am-5pm";
         
         <!--[if lte IE 9]><link rel="stylesheet" type="text/css" href="css/ie.css" media="screen" /><![endif]-->
         <link href="css/custom.css" rel="stylesheet" type="text/css">
-        <link href="/Template/css/css-onlyCarousel2.css" rel="stylesheet">
-        <link href="/Template/css/responsive3.css" rel="stylesheet">
-        <link href="/Template/css/actionbar2.css" rel="stylesheet">
-        <link href="/Template/css/topnavbar.css" rel="stylesheet">
+        <link href="css/css-onlyCarousel2.css" rel="stylesheet">
+        <link href="css/actionbar2.css" rel="stylesheet">
+        <link href="css/topnavbar.css" rel="stylesheet">
         	    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-                <link href="/Template/css/offcanvasmenu2.css" rel="stylesheet">
-        
+                <link href="css/offcanvasmenu2.css" rel="stylesheet">
+                <link href="css/responsive3.css" rel="stylesheet">
        
 
 
