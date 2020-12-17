25957d6a4491034ab1d6827d3f7a9602c300137f
Johnny Platinum <johnnyplatinum178@gmail.com>
2020-12-17 13:28:21 -0500

    z-index

--- index.php
+++ index.php
@@ -30,13 +30,14 @@ $workinghours="Mon-Sat 9am-5pm";
         <link href="css/style.css" rel="stylesheet" type="text/css">
         
         <!--[if lte IE 9]><link rel="stylesheet" type="text/css" href="css/ie.css" media="screen" /><![endif]-->
-        <link href="css/custom.css" rel="stylesheet" type="text/css">
+        
         <link href="css/css-onlyCarousel2.css" rel="stylesheet">
         <link href="css/actionbar2.css" rel="stylesheet">
         <link href="css/topnavbar.css" rel="stylesheet">
-        	    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-                <link href="css/offcanvasmenu2.css" rel="stylesheet">
-                <link href="css/responsive3.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+  integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link href="css/offcanvasmenu2.css" rel="stylesheet">
+        <link href="css/responsive3.css" rel="stylesheet">
        
 
 
